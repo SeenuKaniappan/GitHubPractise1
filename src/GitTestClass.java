98f2c0698408433690c25f7f5d0ff7bfbbca7744
seenuqa13 <seenuqa13@gmail.com>
2021-07-01 18:58:54 +0530

    New

--- src/GitTestClass.java
+++ src/GitTestClass.java
@@ -5,6 +5,7 @@ public class GitTestClass {
 		// TODO Auto-generated method stub
 	//This change is done from the remote repository
 		//This change is also done from remote repository
+		//This changes has been done for local repo to remote
 	}
 
 }
